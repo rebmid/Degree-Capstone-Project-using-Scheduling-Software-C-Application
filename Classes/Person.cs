4621bef74225cbbb7b800405de04244965bfc07a
Rebekah Midkiff <rebmid@microsoft.com>
2022-06-02 14:38:05 -0700

    Add project files.

---
+++ Classes/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleProgram.Classes
+{
+    public abstract class Person
+    {
+        public byte Active { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+
+}
+
---
+++ Classes/User.cs
@@ -0,0 +1,33 @@
+﻿using C969Rebekah.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleProgram.Classes
+{
+    public class User : Person
+    {
+        public int UserID;
+        public string UserName;
+        public string Password;
+        //public byte Active;
+        //public DateTime CreateDate;
+        //public string CreatedBy;
+        //public DateTime LastUpdate;
+        //public string LastUpdateBy;
+
+        public User(int userID, string userName, string password, int active, DateTime createDate)
+        {
+            UserID = userID;
+            UserName = userName;
+            Password = password;
+            Active = (byte)active;
+            CreateDate = createDate;
+         
+        }
+    }
+}
+
